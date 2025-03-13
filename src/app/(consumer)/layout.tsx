304268cc72fc248d7a3cb94af5d0f56dc11e976b
Ayush Patel <aayushpatel25190@gmail.com>
2025-03-13 12:08:29 +0530

    Add authentication pages, layout, and utility functions; update global styles and dependencies
    - Add sign in, sign up route.
    - Create Consumer layout.
    - Add shadcn dependcies: It will also update the global.css by itself and add 'component.json', 'util.ts' file.
    - Add shadcn button component
    - Update the global css by adding background and foreground color variable in theme and update the 'container' class of tailwind css

---
+++ src/app/(consumer)/layout.tsx
@@ -0,0 +1,77 @@
+import { Button } from '@/components/ui/button';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import { ReactNode, Suspense } from 'react';
+
+export default function ConsumerLayout({
+  children
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
+
+function Navbar()  {
+  return  (
+    <header
+      className='flex h-12 shadow bg-background z-10'
+    >
+      <nav
+        className='flex gap-4 container'
+      >
+        <Link
+          href='/'
+          className='mr-auto text-lg hover:underline px-2 flex items-center'
+        >
+          Learnify
+        </Link>
+        
+        <Suspense>
+          <SignedIn>
+            <Link
+              href='/admin'
+              className='hover:bg-accent/10 flex items-center px-2'
+            >
+              Admin
+            </Link>
+            <Link
+              href='/courses'
+              className='hover:bg-accent/10 flex items-center px-2'
+            >
+              My Courses
+            </Link>
+            <Link
+              href='/purchases'
+              className='hover:bg-accent/10 flex items-center px-2'
+            >
+              Purchases History
+            </Link>
+            <div className='size-8 self-center'>
+              <UserButton
+                appearance={{
+                  elements: {
+                    userButtonAvatarBox: { width: '100%', height: '100%'}
+                  }
+                }}
+              />
+            </div>
+          </SignedIn>
+        </Suspense>
+
+        <Suspense>
+          <SignedOut>
+            <Button
+              className='self-center'
+              asChild
+            >
+              <SignInButton>Sign In</SignInButton>
+            </Button>
+          </SignedOut>
+        </Suspense>
+      </nav>
+    </header>
+  );
+}
